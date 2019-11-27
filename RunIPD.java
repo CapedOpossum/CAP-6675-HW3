57ba0aa7a77e3c173b572556a8912cc2d07c8666
Rolando Nieves <rjnieves@athena.domain>
2019-11-27 11:13:17 -0500

    Migrate to StrategyFactory
    Instead of hard-coding the strategies used by each player, let the
    strategies be specified at the command line, with all strategies
    available via a newly-created factory class, and identified by name.

--- RunIPD.java
+++ RunIPD.java
@@ -3,39 +3,51 @@
  * iterated Prisoner's Dilemma (IPD).
  * @author	081028AW
  */
-public class RunIPD extends Object
-   {
-  /**
-   * Main program to start IPD program.
-   */
+public class RunIPD extends Object {
+    /**
+     * Main program to start IPD program.
+     */
 
-   public static void main(String args[])
-      {
-      int i;
-      int maxSteps = 0;
+    public static void main(String args[]) {
+        int i;
+        int maxSteps = 0;
 
-      Strategy player1, player2;
-      IteratedPD ipd;
+        Strategy player1 = null;
+        Strategy player2 = null;
+        IteratedPD ipd;
 
-      for (i=0; i<args.length; i++)
-         {
-        /* check parameters */
-         if (args[i].equals("-l") || args[i].equals("-L"))
-            {
-            maxSteps = Integer.parseInt(args[i+1]);
-            System.out.println(" Max steps = " + maxSteps);
-            }  /* if */
-         }  /* for i */
+        for (i = 0; i < args.length; i++) {
+            /* check parameters */
+            if (args[i].equals("-l") || args[i].equals("-L")) {
+                maxSteps = Integer.parseInt(args[++i]);
+                System.out.println(" Max steps = " + maxSteps);
+            } else if (args[i].equals("-p1") || args[i].equals("-P1")) {
+                player1 = StrategyFactory.createFromName(
+                    args[++i]
+                );
+            } else if (args[i].equals("-p2") || args[i].equals("-P2")) {
+                player2 = StrategyFactory.createFromName(
+                    args[++i]
+                );
+            }
+        }
 
-      player1 = new StrategyTitForTwoTats();
-      player2 = new StrategyTitForTat();
-      ipd = new IteratedPD(player1, player2);
+        if (null == player1) {
+            System.err.println("Invalid strategy for P1.");
+            System.exit(-1);
+        }
 
-      ipd.runSteps(maxSteps);
+        if (null == player2) {
+            System.err.println("Invalid strategy for P2.");
+            System.exit(-1);
+        }
 
-      System.out.printf(" Player 1 score = %d\n", ipd.player1Score());
-      System.out.printf(" Player 2 score = %d\n", ipd.player2Score());
+        ipd = new IteratedPD(player1, player2);
 
-      }  /* main */
-   }  /* class RunIPD */
+        ipd.runSteps(maxSteps);
 
+        System.out.printf(" Player 1 score = %d\n", ipd.player1Score());
+        System.out.printf(" Player 2 score = %d\n", ipd.player2Score());
+
+    } /* main */
+} /* class RunIPD */
---
+++ StrategyFactory.java
@@ -0,0 +1,26 @@
+/**
+ * Factory to create instances of all available strategies.
+ *
+ * Strategies may be created by using their identifying character string name.
+ *
+ * @author Rolando J. Nieves
+ */
+public class StrategyFactory {
+    public static Strategy createFromName(String strategyName) {
+        if (strategyName.equals("Random")) {
+            return new StrategyRandom();
+        } else if (strategyName.equals("AlwaysCooperate")) {
+            return new StrategyAlwaysCooperate();
+        } else if (strategyName.equals("AlwaysDefect")) {
+            return new StrategyAlwaysDefect();
+        } else if (strategyName.equals("TitForTat")) {
+            return new StrategyTitForTat();
+        } else if (strategyName.equals("TitForTwoTats")) {
+            return new StrategyTitForTwoTats();
+        } else {
+            return null;
+        }
+    } // end createFromName()
+} // end StrategyFactory
+
+// vim: set ts=4 sw=4 expandtab:
